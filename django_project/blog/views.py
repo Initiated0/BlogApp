cea6e3a7cfe0a64604d834e369108edc0558587b
initiate0 <damonmayeehamm@gmail.com>
2021-04-24 22:46:19 +0600

    CSS styles are added

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,12 +1,31 @@
 from django.shortcuts import render
 
+posts = [
+	{
+		'author': 'nayeem',
+		'title': 'Blog post one',
+		'content': 'First post content',
+		'date_posted': 'August 27, 2018'
 
+	},
+	{
+		'author': 'doe',
+		'title': 'Blog post two',
+		'content': 'Second post content',
+		'date_posted': 'August 2f, 2018'
+
+	}
+
+]
 
 def home(request):
-    return render(request, 'blog/home.html')
+	context = {
+		'posts': posts
+	}
+	return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return render(request, 'blog/about.html')
+    return render(request, 'blog/about.html', {'title': 'GOGO Company'})
 
  
